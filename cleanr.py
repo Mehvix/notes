9634d4db4419997dfe5722c67d3efa6ed35c48f4
Max Vogel <max-v@berkeley.edu>
2022-05-27 13:48:32 -0700

    upd cleanr

--- cleanr.py
+++ cleanr.py
@@ -1,18 +1,68 @@
 #!/usr/bin/env python3
 import os
-import re
 import sys
+import re
+import argparse
+import urllib.parse
 
+COURSE_PLACEHOLDER = "@COURSENAME"
+DEFAULT_FIN = "unclean.md"
+DEFAULT_FOUT = "clean.md"
+COURSES = {"c100", "16a", "e-29", "c61"}
 
-def decode(c):
-    return hex(ord(c))
+parser = argparse.ArgumentParser()
+parser.add_argument("--course",
+                    "-c",
+                    help="Specifies course",
+                    default=None,
+                    required=False)
+parser.add_argument("--fin",
+                    "-i",
+                    help="Specifies input file. Default: " + DEFAULT_FIN,
+                    type=argparse.FileType('r'))
+parser.add_argument("--fout",
+                    "-o",
+                    help="Specifies output file Default: " + DEFAULT_FOUT,
+                    type=argparse.FileType('r'))
+parser.add_argument("--force",
+                    "-f",
+                    help="Force overwrite output file, if it already exists",
+                    action="store_true")
+parser.add_argument("--base",
+                    "-B",
+                    help="Does not use base rules",
+                    action="store_true")
+parser.add_argument("--header",
+                    "-H",
+                    help="Does not use header rules",
+                    action="store_true")
+parser.add_argument("--verbose",
+                    "-v",
+                    action="store_true",
+                    help="increase output verbosity")
 
+args = parser.parse_args()
+
+if not args.verbose:
+    print = lambda: ()
+
+print("Verbose on!")
+
+FIN = args.fin.name if args.fin else DEFAULT_FIN
+FOUT = args.fout.name if args.fout else DEFAULT_FOUT
+COURSE = args.course or COURSE_PLACEHOLDER
 
 DIR = os.path.dirname(os.path.realpath(__file__)) + "/"
+DIN = DIR + FIN
+DOUT = DIR + FOUT
 
+# Rules
+# todo make this pretty
 BASE = [
     (r"$", r""),  # https://stackoverflow.com/a/10913081/7833617
-    (r" [–-]{1,2} ", r" -- "),
+    (r" ?[–-—]{1,2} ?", r" -- "),
+    (r"([Ll]eft)", r"(L)"),
+    (r"([Rr]ight)", r"(R)"),
     # ======= #
     # Bullets #
     # ======= #
@@ -27,22 +77,6 @@ BASE = [
     (r"\)\(", r")\n\t- ("),  # Parenthesis Unjoin (i.e sources)
     (r"\n\(", r" ("),  # Parenthesis 2
     (r"([^\n]{0,55} ?)\n( ?[a-z&(])", r"\1 \2"),  # word-wrap
-    # ======= #
-    # Headers #
-    # ======= #
-    (r"^[☛]", r"## "),  # H2 unique
-    (r"(^[A-Z][^ .].{2,42}\n)", r"\n## \1\n"),  # Headers
-    (r"#+\s*(.*\n+)#+\s*(.*\n+)#+\s*(.*\n+)", r"# \1## \2### \3"),  # H3s
-    (r"#{1,2}\s*(.*\n+)#{2,3}\s*(.*\n+)", r"# \1## \2"),  # H2s
-    (r"(#.*?)\n+\s*. ([A-Z0-9\"])", r"\1\n\n- \2"),  # spacing after headers
-    (r"$\n+#", r"\n\n#"),  # spacing before headers
-    (r"(#.*$\n*)\t+(.)", r"\1\2"),  # normalize below header
-    # (r"(#.*$\n+[*-].*$)(?:\n\s(. .*))+", r"\1\n\t\2"),  # normalize two down
-    (r"^([\s]*). (\d+\.)", r"\1\2"),  # point before numbers
-    (r"^([A-Z].{20,})", r"- \1"),  # long lines
-    (r'^(\s*. )(.{3,45}?)(: *[^"])', r"\1**\2**\3"),  # term/definition
-    (r"[““””]", r'"'),
-    (r"\n([,.])", r" \1"),
     # ========== #
     # Whitespace #
     # ========== #
@@ -61,57 +95,83 @@ BASE = [
     (r"^(\d+\. .*\n)- ", r"\1\t- "),  # indent fater num lists
     (r" {2,}", r" "),
     (r"\n\n\t(.\s*)\b", r"\n\t\1"),
-    (r"^(.){1,3}$", r"<!--\1-->"),  # remove lines with three or less characters
+    (r"^(.){1,3}$",
+     r"<!--\1-->"),  # remove lines with three or less characters
     (r"^\s*\n(\t+. .)", r"\1"),  # gaps between bps
-    (r" {1,3}\t",r"    "), # hidden tab
-    (r"\t",r"    "), # tab to four spaces
+    (r" {1,3}\t", r"    "),  # hidden tab
+    (r"\t", r"    "),  # tab to four spaces
 ]
 
-STYLES = dict([(k, []) for k in {"C100", "16A", "E29"}])
-STYLES["C100"] = [
+if not args.header:
+    BASE += [
+        # ======= #
+        # Headers #
+        # ======= #
+        (r"^[☛]", r"## "),  # H2 unique
+        (r"(^[A-Z][^ .].{2,42}\n)", r"\n## \1\n"),  # Headers
+        (r"#+\s*(.*\n+)#+\s*(.*\n+)#+\s*(.*\n+)", r"# \1## \2### \3"),  # H3s
+        (r"#{1,2}\s*(.*\n+)#{2,3}\s*(.*\n+)", r"# \1## \2"),  # H2s
+        (r"(#.*?)\n+\s*. ([A-Z0-9\"])",
+         r"\1\n\n- \2"),  # spacing after headers
+        (r"$\n+#", r"\n\n#"),  # spacing before headers
+        (r"(#.*$\n*)\t+(.)", r"\1\2"),  # normalize below header
+        # (r"(#.*$\n+[*-].*$)(?:\n\s(. .*))+", r"\1\n\t\2"),  # normalize two down
+        (r"^([\s]*). (\d+\.)", r"\1\2"),  # point before numbers
+        (r"^([A-Z].{20,})", r"- \1"),  # long lines
+        (r'^(\s*. )(.{3,45}?)(: *[^"])', r"\1**\2**\3"),  # term/definition
+        (r"[““””]", r'"'),
+        (r"\n([,.])", r" \1")
+    ]
+
+STYLES = dict([(k, []) for k in COURSES])
+STYLES["c100"] = [
     (r"\n[““””]", r' "'),
     (r'^\s*((?:.|\d+\.)? ?)(♫|(?:[“"].*[”"].*)$)', r"\t\1> \2"),  # quotes
     (r"^(\s)*.(> )", r"\1\2"),  # botch
     (r" v ", r"\n\t- "),
 ]
-STYLES["16A"] = [(r"~([\\a-Z])", r"\vec \1")]
-STYLES["E29"] = [
+STYLES["16a"] = [(r"~([\\a-Z])", r"\vec \1")]
+STYLES["e-29"] = [
     ("[\uE000-\uF8FF]", r"-"),
     (r"[•]", r"-"),
     (r"[▪]", r"\t-"),
 ]
+STYLES["c61"] = [
+    (r"^ ?Figure(?:\n*| )((\d+)\.(\d+)\..*?)$",
+     r"\n{{< columns >}}<!-- mathjax fix -->\n" + r"\n" +
+     r"<---><!-- mathjax fix -->\n" + f">![](/docs/mcb-c61/\\2\\3.png)\n" +
+     r">Figure \1\n" + r"{{< /columns >}}\n"),
+    (
+        r"((?:[A-Z][a-z]{2,} ?){2}) (\([12]\d{3}-[12]\d{3}\))",
+        r"[\1 \2](https://en.wikipedia.org/wiki/\1)",
+    ),
+    (r"^ ?([A-Z])", r"- \1"),
+    (r'([a-zA-Z])[.?!]"? ', r"\1\n\t- "),
+]
 
 
-def main(fname, flags):
-    styling = "DEFAULT ONLY"
-    styling = next((flag for flag in flags if flag in STYLES.keys()), None)
-    print("STYLING: " + styling)
+def decode(c):
+    return hex(ord(c))
 
-    copy = fname.split(".")[0] + "-clean.md"
-    if (
-        "-d" not in flags
-        and os.path.isfile(copy)
-        and "Y"
-        != input(
-            "clean copy exists, do you want to overwrite and continue? [y/n]\n"
-        ).upper()[0]
-    ):
+
+def main():
+    if (not args.force and os.path.isfile(DOUT) and "Y" !=
+        (input(f"> overwrite {FOUT}? [y/*]\n").upper() + "_")[0]):
         exit()
 
-    with open(fname, "r") as fin:
+    rules = BASE if not args.base else []
+    if COURSE and COURSE in STYLES.keys():
+        rules += STYLES.get(COURSE)
+
+    with open(DIN, "r") as fin:
         data = fin.read().strip()
-        with open(copy, "w") as fout:
-            for old, new in STYLES[styling] + BASE:
-                print(old)
-                data = re.sub(old, new, data, flags=re.M)
+        with open(DOUT, "w") as fout:
+            for pat, repl in rules:
+                data = re.sub(pat, repl, data, flags=re.M)
+                print(f"{[pat]} => {[repl]}")
+
             fout.write(data)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 1:
-        exit("filename needs to be passed in")
-    if not os.path.isfile(DIR + sys.argv[1]):
-        exit("file passed in DNE")
-    if str(sys.argv[1]).count(".") > 1:
-        exit("filename cannot have periods")
-    main(DIR + sys.argv[1], sys.argv[2:])
+    main()
